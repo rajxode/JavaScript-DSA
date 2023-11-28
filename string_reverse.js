8f0c0b65230a3fce8a2eab951d38dd82c61392a1
Rajeev Kumar Sharma <120395470+rajxode@users.noreply.github.com>
2023-11-28 15:31:59 +0000

    string reverse comments

--- string_reverse.js
+++ string_reverse.js
@@ -2,21 +2,29 @@
 // Reverse a string using for loop
 
 const str = 'abcd';
+// to store the reverse string
 var revStr = '';
 
+// looping over each element in reverse order
 for(let i = str.length - 1; i >= 0; i-- ){
+    // add the character of str into revStr
     revStr = revStr + str[i];
 }
 
+// show the reverse string
 console.log('Reverse String: ', revStr);
 
 
 // Reverse string using inbuilt methods
 
+// convert the string into an array
 const splitStr = str.split('');
 
+// reverse the array 
 const revSplitStr = splitStr.reverse();
 
+// join the array into string again
 const revStr2 = revSplitStr.join('');
 
+// show the reverse string
 console.log('Reverse String ( 2nd method ): ',revStr2);
